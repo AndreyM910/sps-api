5299f3b50281b3c7966cb1cae2121ae5296a7b11
Andrey M <andrey.musatov.ua@gmail.com>
2021-03-24 04:39:37 +0200

    create config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot({
       autoSchemaFile: 'src/schema.gql',
     }),
-    MongooseModule.forRoot('mongodb://localhost:26017/sps-app'),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DBNAME}`
+    ),
     UserModule,
     AuthModule,
   ],
